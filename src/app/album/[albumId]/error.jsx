6c308c670552d65f620d965208b83ff78bb2ab2c
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-23 19:11:31 +0530

    loading page

---
+++ src/app/album/[albumId]/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const error = () => {
+  return (
+    <div className="text-white w-screen h-screen flex justify-center items-center">
+        <h1 className="text-4xl font-bold">Error Occured</h1>
+    </div>
+  )
+}
+
+export default error
