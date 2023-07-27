b2f9d5ef520afc3385ba542ffd7d90b0f8012bcf
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-07-27 14:20:41 +0530

    track

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Track = ({ isPlaying, isActive, activeSong, fullScreen }) => (
   <div className={`flex-1 flex items-center justify-start ${fullScreen ? 'hidden':''}`}>
     <div className={`${isPlaying && isActive ? 'animate-[spin_15s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
-      <img src={activeSong?.image?.[2].link} alt="cover art" className="rounded-full"/>
+      <img src={activeSong?.image?.[2].link || 'https://res.cloudinary.com/dbr73rpz9/image/upload/v1690447758/chibi_qxvxq6.png'} alt="cover art" className="rounded-full"/>
     </div>
     <div className={`w-[190px] select-none cursor-pointer`}>
       <p className="truncate text-white font-bold text-lg">
