24dcf3b654a640f35b44320c0ed35a1f95d53a34
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-01 17:24:52 +0530

    not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const notfound = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen overflow-hidden text-white'>
+        <h1 className='text-9xl font-bold'>404</h1>
+        <h2 className='text-2xl font-bold'>Page Not Found</h2>
+    </div>
+  )
+}
+
+export default notfound
