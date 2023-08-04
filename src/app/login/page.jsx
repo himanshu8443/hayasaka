63af61d5f3df4bbd044a33efdb4a47016dea5744
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-04 21:07:23 +0530

    login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,77 @@
+'use client'
+import React from 'react'
+import { useState } from 'react'
+import Link from 'next/link';
+import { signIn } from 'next-auth/react';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
+import { setProgress } from '@/redux/features/loadingBarSlice';
+
+const page = () => {
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+
+    const onchange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    };
+
+    const handelSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            dispatch(setProgress(70));
+            const res = await signIn('credentials', {
+                redirect: false,
+                email: formData.email,
+                password: formData.password
+            });
+            if (!res.error) {
+                toast.success('Logged in successfully');
+                router.push('/dashboard');
+            }
+            else {
+                toast.error('Invalid credentials');
+            }
+        } catch (error) {
+            toast.error(error?.message);
+        } finally {
+            dispatch(setProgress(100));
+        }
+    };
+  return (
+    <div className=' w-11/12 mx-auto mt-32 min-h-screen'>
+            <div className=" flex justify-center items-center">
+                <div className="container flex justify-center flex-col items-center w-1/2">
+                    <h1 className=" text-4xl text-cyan-400 font-medium mb-8">Login</h1>
+                    <form onSubmit={handelSubmit} className="text-white flex flex-col text-xl gap-5 font-medium">
+                        <div className=" flex gap-4 items-end">
+                            <label  className=" mr-11" htmlFor='email'>Email</label>
+                            <input onChange={onchange} value={formData.email} name='email' type="email" placeholder="Email" required className=' appearance-none bg-black border-b border-white focus:outline-none' />
+                        </div>
+                        <div className=" flex gap-4 items-end">
+                            <label className="" htmlFor='password'>Password</label>
+                            <input onChange={onchange} value={formData.password} name='password' type="password" placeholder="Password" required className=' appearance-none bg-black border-b border-white focus:outline-none' />
+                        </div>
+                        <div className=" w-full flex justify-center">
+                            <button type='submit' className="relative inline-block px-4 py-2 font-medium group">
+                                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-cyan-500 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                                <span className="absolute inset-0 w-full h-full bg-black border-2 border-white group-hover:bg-"></span>
+                                <span className="relative text-white group-hover:text-cyan-400">Login</span>
+                            </button>
+                        </div>
+                        <p className=" w-full flex justify-center gap-2">
+                            Don't have an account? <Link href={'/signup'} className=' text-cyan-400 font-semibold'> Sign Up</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default page
