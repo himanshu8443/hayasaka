368519482f727987baa085e86c3518fcf3200c86
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-05 18:47:51 +0530

    User Models

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+
+const fileSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    resetPasswordToken : {
+        type: String,
+        default: null
+    },
+    resetPasswordExpires : {
+        type: Date,
+        default: null
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    verificationToken: {
+        type: String,
+        default: null
+    },
+    verificationTokenExpires: {
+        type: Date,
+        default: null
+    },
+    userData: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "userData"
+    }
+}, { timestamps: true }
+);
+
+export default mongoose.models.user || mongoose.model("user", fileSchema, "user");
---
+++ src/models/UserData.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema(
+    {
+        favourites: {
+            type: Array,
+            default: []
+        },
+        songHistory: {
+            type: Array,
+            default: []
+        }
+    },
+    { timestamps: true }
+);
+
+export default mongoose.models.userData || mongoose.model("userData", fileSchema, "userData");
