2c1734b6e750133cc1b68a5d2c0792291b60311e
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-07 08:51:43 +0530

    mail sender

---
+++ src/utils/mailSender.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+
+const mailSender = async (email, title, body) => {
+    try{
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                port: 587,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
+
+
+            let info = await transporter.sendMail({
+                from: `"Hayasaka" <${process.env.MAIL_USER}>`,
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error.message);
+        return error;
+    }
+}
+
+export default mailSender;
+
