bb706bbc5039e71a854bdc8249fc66e2e0809891
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-11 09:20:07 +0530

    fix checkbox

--- src/components/MusicPlayer/Lyrics.jsx
+++ src/components/MusicPlayer/Lyrics.jsx
@@ -61,12 +61,13 @@ const Lyrics = ({ activeSong }) => {
                                 Auto add similar songs to queue
                             </p>
 
-                            <label className="relative inline-flex items-center mb-1 cursor-pointer mr-4">
+                            <label htmlFor='autoAdd' className="relative inline-flex items-center mb-1 cursor-pointer mr-4">
                                 <input onChange={
                                     (e) => {
                                         handleAutoAdd(e.target.checked)
                                     }
-                                } type="checkbox" defaultChecked={autoAdd} className="sr-only peer"/>
+                                } type="checkbox" defaultChecked={autoAdd} className="sr-only peer" name='autoAdd' id='autoAdd' placeholder=''>
+                                </input>
                                     <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none ring-2  ring-gray-500 ch rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-[#00e6e6]"></div>
                             </label>
 
