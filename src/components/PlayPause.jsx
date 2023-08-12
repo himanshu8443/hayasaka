5a8e3f750abe0b654d8e4240ac02d3601199091a
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-08-12 08:46:51 +0530

    Hover effect

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -8,13 +8,13 @@ const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay, loadi
   isPlaying && activeSong?.name === song.name ? (
   <FaPauseCircle
     size={35}
-    className="text-gray-300"
+    className="text-gray-300 hover:scale-125 transform transition-all duration-300 ease-in-out"
     onClick={handlePause}
   />
 ) : (
   <FaPlayCircle
     size={35}
-    className="text-gray-300"
+    className="text-gray-300 hover:scale-125 transform transition-all duration-300 ease-in-out"
     onClick={handlePlay}
   />
 ));
