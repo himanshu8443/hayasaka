ebe2a12a6045fda37b9ff25a9af7183fb19b8e04
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-09-21 15:25:43 +0530

    Toturial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,47 @@
+'use client'
 import Homepage from '@/components/Homepage/Home'
+import { useEffect, useLayoutEffect } from 'react'
+import { useState } from 'react'
 
 export default function Home() {
+  const [showtip, setShowtip] = useState(false);
+  const [toturialComplete, setToturialComplete] = useState(false);
+
+  useLayoutEffect(() => {
+    setToturialComplete(JSON.parse(localStorage.getItem('toturialComplete')));
+
+    setTimeout(() => {
+      if (!toturialComplete) {
+        setShowtip(true);
+      }
+    }, 5000);
+  }, [])
+  useEffect(() => {
+
+  }, [toturialComplete])
+
+  const handleClick = () => {
+    setShowtip(false);
+    setToturialComplete(true);
+    localStorage.setItem('toturialComplete', true);
+  }
   return (
     <div>
+      {showtip && !toturialComplete &&
+        <div className='absolute top-4 left-16 z-50'>
+          <div className=' relative bg-[#3a3b3b] p-4 rounded-lg'>
+            <p className=' text-lg text-gray-300'>
+              Create your own <span className='text-[#00e6e6]'>Playlists</span> <br />and
+              add songs to <span className='text-[#00e6e6]'>Favourite</span>
+            </p>
+            <div className=' flex items-center mt-2 justify-end'>
+              <button onClick={handleClick} className=' bg-[#00e6e6] text-black px-3 py-2 rounded-lg'>Ok</button>
+            </div>
+            <div className=' absolute top-2 -left-2 bg-[#3a3b3b] rotate-[50deg] w-6 h-6'></div>
+          </div>
+        </div>}
       <div className=' mx-auto relative flex flex-col w-11/12 text-white '>
-
-      <Homepage/>
-      
+        <Homepage />
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 'use client'
 import React from 'react'
-import  logo from '../assets/hayasaka.png'
+import logo from '../assets/hayasaka.png'
 import Image from 'next/image'
 import Searchbar from './Searchbar'
 import Link from 'next/link'
 import { useDispatch } from 'react-redux'
 import { setProgress } from '@/redux/features/loadingBarSlice'
-import {MdOutlineMenu} from 'react-icons/md'
+import { MdOutlineMenu } from 'react-icons/md'
 import { IoClose } from 'react-icons/io5'
 import Sidebar from './Sidebar/Sidebar'
 
@@ -16,31 +16,30 @@ const Navbar = () => {
   const [showNav, setShowNav] = React.useState(false);
   return (
     <>
-    <div className='navbar h-[70px] text-white flex justify-between relative'>
-      <div className=' flex'>
-      <MdOutlineMenu onClick={
-        ()=>setShowNav(true)
-      } className=' mx-4 text-2xl lg:text-3xl my-auto cursor-pointer' />
-      <div className=' flex justify-center items-center'>
-        <Link href='/'>
-      <Image onClick={()=>{dispatch(setProgress(100))}} 
-      src={logo} alt="logo"  className=' lg:py-2  aspect-video w-[135px] h-[30.741px] lg:h-[58px] lg:w-[190px]'/>
-      </Link>
+      <div className='bg-[#020813] h-[70px] text-white flex justify-between relative'>
+        <div className=' flex'>
+          <MdOutlineMenu onClick={
+            () => setShowNav(true)
+          } className=' mx-4 text-2xl lg:text-3xl my-auto cursor-pointer' />
+          <div className=' flex justify-center items-center'>
+            <Link href='/'>
+              <Image onClick={() => { dispatch(setProgress(100)) }}
+                src={logo} alt="logo" className=' lg:py-2  aspect-video w-[135px] h-[30.741px] lg:h-[58px] lg:w-[190px]' />
+            </Link>
+          </div>
+        </div>
+        <Searchbar />
       </div>
+
+      <Sidebar showNav={showNav} setShowNav={setShowNav} />
+      {/* overlay */}
+      <div onClick={() => setShowNav(false)}
+        className={`${showNav ? '' : 'hidden'} transition-all duration-200 fixed top-0 left-0 z-30 w-screen h-screen bg-black bg-opacity-50`}></div>
+      <div onClick={
+        () => setShowNav(false)
+      } className={`${showNav ? '' : 'hidden'} md:hidden fixed top-7 right-10 z-50 text-3xl text-white`}>
+        <IoClose />
       </div>
-      <Searchbar/>
-    </div>
-    
-    <Sidebar showNav={showNav} setShowNav={setShowNav} />
-    {/* overlay */}
-    <div onClick={()=>setShowNav(false)} 
-    className={`${showNav ? '':'hidden' } transition-all duration-200 fixed top-0 left-0 z-30 w-screen h-screen bg-black bg-opacity-50`}></div>
-    {/* cross */}
-    <div onClick={
-      ()=>setShowNav(false)
-    } className={`${showNav ? '':'hidden' } md:hidden fixed top-7 right-10 z-50 text-3xl text-white`}>
-      <IoClose/>
-    </div>
     </>
   )
 }
