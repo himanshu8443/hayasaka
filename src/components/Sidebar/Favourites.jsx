dd71ee1c4a59b210ff0c82003fd601f99fea8e5b
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-02-09 19:05:49 +0530

    Refactor Sidebar and add styles

--- src/components/Sidebar/Favourites.jsx
+++ src/components/Sidebar/Favourites.jsx
@@ -1,17 +1,27 @@
-import Link from 'next/link'
-import React from 'react'
-import { AiFillHeart } from 'react-icons/ai'
+import Link from "next/link";
+import React from "react";
+import { AiFillHeart } from "react-icons/ai";
 
-
-const Favourites = ({setShowNav}) => {
+const Favourites = ({ setShowNav }) => {
   return (
-    <div className=' mt-7 border-b border-gray-400 w-[95%] '>
-    <Link href='/favourite' className='flex cursor-pointer items-center' onClick={()=>setShowNav(false)}>
-    <p className=' font-semibold text-lg text-white mx-3 mb-7'>Favourites</p>
-    <AiFillHeart title='Favourites' size={25} color={'white'} className={` mb-7 `} />
-    </Link>
+    <div className=" pt-5 m-2 rounded-md w-[95%] hover:bg-white/5">
+      <Link
+        href="/favourite"
+        className="flex cursor-pointer items-center"
+        onClick={() => setShowNav(false)}
+      >
+        <p className=" font-semibold text-lg text-white mx-3 mb-7">
+          Favourites
+        </p>
+        <AiFillHeart
+          title="Favourites"
+          size={25}
+          color={"white"}
+          className={` mb-7 `}
+        />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Favourites
+export default Favourites;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,52 +1,76 @@
-import React from 'react'
-import  logoWhite from '../../assets/logoWhite.png'
-import Languages from './Languages'
-import Favourites from './Favourites'
-import { FaGithub } from 'react-icons/fa'
-import {MdOutlineMenu} from 'react-icons/md'
-import Image from 'next/image'
-import Link from 'next/link'
-import Profile from './Profile'
-import { useDispatch } from 'react-redux'
-import Playlists from './Playlists'
-import { setProgress } from '@/redux/features/loadingBarSlice'
+import React from "react";
+import logoWhite from "../../assets/logoWhite.png";
+import Languages from "./Languages";
+import Favourites from "./Favourites";
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineMenu } from "react-icons/md";
+import Image from "next/image";
+import Link from "next/link";
+import Profile from "./Profile";
+import { useDispatch } from "react-redux";
+import Playlists from "./Playlists";
+import { setProgress } from "@/redux/features/loadingBarSlice";
 
-
-const Sidebar = ({showNav, setShowNav}) => {
-  const dispatch = useDispatch()
+const Sidebar = ({ showNav, setShowNav }) => {
+  const dispatch = useDispatch();
   return (
-    <div className={`${showNav ? '':'translate-x-[-100%]' } transition-all duration-200  h-screen lg:w-[300px] md:w-[250px] w-[65vw] fixed top-0 left-0 z-40 bg-[#020813] flex flex-col justify-between`}>
-    <div>
-    <div className=' flex mt-3'>
-      <MdOutlineMenu onClick={()=>setShowNav(false)} className=' mx-4 text-2xl lg:text-3xl my-auto text-white cursor-pointer' />
-      <div className=' flex justify-center items-center'>
-      <Link href='/'>
-      <Image onClick={()=>{dispatch(setProgress(100))}} src={logoWhite} alt="logo" 
-      className=' lg:py-2  aspect-video w-[139px] h-[31px] lg:h-[62px] lg:w-[190px]'/>
-      </Link>
-      </div>
-      </div>
-      <div className=' mt-7 pb-7 border-b border-gray-400 w-[95%]'>
-        <Profile setShowNav={setShowNav}/>
+    <div
+      className={`${
+        showNav ? "" : "translate-x-[-100%]"
+      } transition-all duration-200  h-screen lg:w-[300px] md:w-[250px] w-[65vw] fixed top-0 left-0 z-40 bg-[#020813] flex flex-col justify-between`}
+    >
+      <div>
+        <div className=" flex mt-3">
+          <MdOutlineMenu
+            onClick={() => setShowNav(false)}
+            className=" mx-4 text-2xl lg:text-3xl my-auto text-white cursor-pointer"
+          />
+          <div className=" flex justify-center items-center">
+            <Link href="/">
+              <Image
+                onClick={() => {
+                  dispatch(setProgress(100));
+                }}
+                src={logoWhite}
+                alt="logo"
+                className=" lg:py-2  aspect-video w-[139px] h-[31px] lg:h-[62px] lg:w-[190px]"
+              />
+            </Link>
+          </div>
         </div>
-      <div className=' mt-7 border-b border-gray-400 w-[95%]'>
-        <Languages/>
+        <div className=" mt-7 pb-7 border-b border-gray-400 w-[95%]">
+          <Profile setShowNav={setShowNav} />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Languages />
+          <hr className="border-gray-400 w-[95%] mx-auto" />
+        </div>
+        <Favourites setShowNav={setShowNav} />
+        <div className="flex flex-col gap-1">
+          <hr className="border-gray-400 w-[95%] mx-auto" />
+          <Playlists setShowNav={setShowNav} />
+          <hr className="border-gray-400 w-[95%] mx-auto" />
+        </div>
       </div>
-     <Favourites setShowNav={setShowNav} />
-     <div className=' mt-7 border-b border-gray-400 w-[95%]'>
-      <Playlists setShowNav={setShowNav}/>
+      <div className=" mb-28 text-gray-200 mx-3 flex gap-3">
+        <Link href="/dmca">
+          <p className="hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm">
+            DMCA
+          </p>
+        </Link>
+        <a
+          href="https://github.com/himanshu8443/hayasaka"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p className=" hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm flex items-center gap-1">
+            <FaGithub />
+            Github
+          </p>
+        </a>
       </div>
-     </div>
-     <div className=' mb-28 text-gray-200 mx-3 flex gap-3'>
-      <Link href='/dmca'>
-      <p className='hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm'>DMCA</p>
-      </Link>
-      <a href='https://github.com/himanshu8443/hayasaka' target='_blank' rel="noreferrer">
-      <p className=' hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm flex items-center gap-1'><FaGithub/>Github</p>
-      </a>
-     </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
