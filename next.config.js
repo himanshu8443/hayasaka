4531611a427b4fcfa494ef30f14459750ac594f1
himanshu8443 <sangwanhimanshu8443@gmail.com>
2024-04-16 09:12:47 +0530

    disable pwa in dev server

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA(nextConfig);
