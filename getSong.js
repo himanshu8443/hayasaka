e67d8b8462eb2c1c2be82684cfca5b569dc6552a
Keerthana T P <tpkeerthana944@gmail.com>
2024-11-02 21:54:43 +0530

    remove

---
+++ getSong.js
@@ -0,0 +1,24 @@
+const searchSong = require('./searchSong');
+const extractLyrics = require('./utils/extractLyrics');
+const { checkOptions } = require('./utils');
+
+/**
+ * @param {{apiKey: string, title: string, artist: string, optimizeQuery: boolean}} options
+ */
+module.exports = async function (options) {
+	try {
+		checkOptions(options);
+		let results = await searchSong(options);
+		if (!results) return null;
+		let lyrics = await extractLyrics(results[0].url);
+		return {
+			id: results[0].id,
+			title: results[0].title,
+			url: results[0].url,
+			lyrics,
+			albumArt: results[0].albumArt
+		};
+	} catch (e) {
+		throw e;
+	}
+};
